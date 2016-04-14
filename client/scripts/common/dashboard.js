3ce6dea77335898240673ef84c06699b8626d265
uinika <uinika@163.com>
2016-04-14 18:02:06 +0800

    Directive base on echarts

--- client/scripts/common/dashboard.js
+++ client/scripts/common/dashboard.js
@@ -28,8 +28,6 @@ Dashboard.controller('Dashboard.Controller.Main', ['$scope',
       'tongjiju'
     ];
 
-
-
   }
 ])
 
@@ -38,7 +36,28 @@ Dashboard.controller('Dashboard.Controller.Main', ['$scope',
 Dashboard.directive('wiservOverviewChart', [
   function() {
     return {
+      restrict: 'ACE',
+      template: "<div style='width:300;height:150px;'></div>",
       link: function(scope, element, attr) {
+        var myChart = echarts.init((element.find('div'))[0]);
+        var option = {
+          tooltip: {},
+          legend: {
+            data: ['销量']
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
 
       }
     };
