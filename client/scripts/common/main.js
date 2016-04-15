eb6988ff0dba0f345563f1e6e3d62be5f84fc2c1
uinika <uinika@163.com>
2016-04-15 15:31:40 +0800

    Tab Switcher works OK

--- client/scripts/common/main.js
+++ client/scripts/common/main.js
@@ -5,9 +5,17 @@ var Main = angular.module('Main', ['ui.router']);
 Main.controller('Main.Controller.Main', ['$scope',
   function($scope) {
     $scope.Tab = {};
-    $scope.Tab.switcher = function(event){
-      console.log(event);
-      $scope.Tab.active = 'active';
+    $scope.Tab.switcher = function(target){
+      switch (target) {
+        case 'dashboard': $scope.Tab.Actived = {};
+          $scope.Tab.Actived.dashboard = 'active'; break;
+        case 'inventory': $scope.Tab.Actived = {};
+          $scope.Tab.Actived.inventory = 'active'; break;
+        case 'requirement': $scope.Tab.Actived = {};
+          $scope.Tab.Actived.requirement = 'active'; break;
+        case 'department': $scope.Tab.Actived = {};
+          $scope.Tab.Actived.department = 'active'; break;
+      }
     };
     $scope.Tab.active = 'active';
   }
