b4dc090e85e5546eadfa38d057d5414c5d4be964
uinika <uinika@163.com>
2016-04-15 10:59:43 +0800

    Update dashboard

---
+++ client/scripts/common/main.js
@@ -0,0 +1,22 @@
+'use strict';
+var Main = angular.module('Main', ['ui.router']);
+
+/** Main Controller */
+Main.controller('Main.Controller.Main', ['$scope',
+  function($scope) {
+    $scope.Switcher={};
+    $scope.Switcher.test=（;
+  }
+])
+
+
+/** Main Controller */
+Main.directive('wiservNavTabSwitcher', [
+  function() {
+    return{
+      link: function(){
+
+      }
+    }
+  }
+])
