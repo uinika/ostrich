566743ad1afc87238cf145d4733d4770e6faf29e
xulan <xulan@chinawiserv.com>
2016-04-16 17:13:33 +0800

    Update publish pages

---
+++ server/api/department/inventoryTotal.js
@@ -0,0 +1,12 @@
+'use strict';
+const Router = require('express').Router(),
+      JsonLoader = require('load-json-file'),
+      Config = require('../../config');
+
+  Router.route('/inventoryTotal')
+    .get(function(request, response) {
+      let head = {}, body = {};
+      let datas = JsonLoader.sync(Config.path + 'department/getInventoryTotal.json');
+      response.json(datas);
+    });
+module.exports = Router;
