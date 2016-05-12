d588c334988848eebb78741ea131eaa2ff61f26c
uinika <uinika@163.com>
2016-05-12 15:05:15 +0800

    Rewrite nav bar

--- client/scripts/common/main.js
+++ client/scripts/common/main.js
@@ -4,19 +4,6 @@ var Main = angular.module('Main', ['ui.router']);
 /** Main Controller */
 Main.controller('Main.Controller.Main', ['$scope',
   function($scope) {
-    $scope.Tab = {};
-    $scope.Tab.switcher = function(target){
-      switch (target) {
-        case 'dashboard': $scope.Tab.Actived = {};
-          $scope.Tab.Actived.dashboard = 'active'; break;
-        case 'dataquota': $scope.Tab.Actived = {};
-          $scope.Tab.Actived.dataquota = 'active'; break;
-        case 'requirement': $scope.Tab.Actived = {};
-          $scope.Tab.Actived.requirement = 'active'; break;
-        case 'department': $scope.Tab.Actived = {};
-          $scope.Tab.Actived.department = 'active'; break;
-      }
-    };
-    $scope.Tab.active = 'active';
+
   }
 ]);
