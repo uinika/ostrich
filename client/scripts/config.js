d204b3a41784636f1327dff7c95a83197c6b7c37
uinika <uinika@163.com>
2016-05-12 17:53:42 +0800

    Update table

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -4,9 +4,9 @@ var Config = angular.module('Config', []);
 
 Config.constant('API', {
   // path: 'http://192.168.13.224:8080/drrp/api' //测试组
-  path:'http://localhost:5001/api' //本机测试
+  //path:'http://localhost:5001/api' //本机测试
   // path: 'http://172.16.1.81:8080/api' //周爽
   // path: 'http://172.16.1.77:8080/api' //吴陶冶
-  // path: 'http://172.16.1.78:8080/api' //米兵
+   path: 'http://172.16.1.78:8080/api' //米兵
   // path: 'http://localhost:8080/api' //VPN测试服务器
 });
