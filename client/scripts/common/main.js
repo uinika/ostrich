7b0b41c27770944eeeee658c60c848884730897a
uinika <uinika@163.com>
2016-05-13 20:58:09 +0800

    Fetch login info from cookie

--- client/scripts/common/main.js
+++ client/scripts/common/main.js
@@ -1,9 +1,9 @@
 'use strict';
-var Main = angular.module('Main', ['ui.router']);
+var Main = angular.module('Main', ['ui.router', 'ngCookies']);
 
 /** Main Controller */
-Main.controller('Main.Controller.Main', ['$scope',
-  function($scope) {
-
+Main.controller('Main.Controller.Main', ['$scope', '$cookies',
+  function($scope, $cookies) {
+    $scope.User = JSON.parse($cookies.get('User'));
   }
 ]);
