935f43507d2b91c74678f94a727e07b82dd49b99
uinika <uinika@163.com>
2016-05-20 10:52:50 +0800

    Remove redundant code

--- client/scripts/data-quota/main.js
+++ client/scripts/data-quota/main.js
@@ -5,9 +5,6 @@ var DataQuota = angular.module('DataQuota', ['ui.router']);
 DataQuota.controller('DataQuota.Controller.Main', ['$scope', '$state', 'DataQuota.Service.Http',
   function($scope, $state, Http) {
 
-    $scope.toggle = function(scope) {
-      scope.toggle();
-    };
     $scope.treeOptions = {
       nodeChildren: "nodes",
       dirSelectable: true,
@@ -22,46 +19,13 @@ DataQuota.controller('DataQuota.Controller.Main', ['$scope', '$state', 'DataQuot
         labelSelected: "a8"
       }
     }
-    $scope.dataForTheTree = [{
-      "name": "Joe",
-      "age": "21",
-      "children": [{
-        "name": "Smith",
-        "age": "42",
-        "children": []
-      }, {
-        "name": "Gary",
-        "age": "21",
-        "children": [{
-          "name": "Jenifer",
-          "age": "23",
-          "children": [{
-            "name": "Dani",
-            "age": "32",
-            "children": []
-          }, {
-            "name": "Max",
-            "age": "34",
-            "children": []
-          }]
-        }]
-      }]
-    }, {
-      "name": "Albert",
-      "age": "33",
-      "children": []
-    }, {
-      "name": "Ron",
-      "age": "29",
-      "children": []
-    }];
+
     // Generated Menu
     Http.menu().then(function(result) {
       if (200 === result.data.head.status) {
         $scope.list = result.data.body;
       }
     })
-
 
   }
 ]);
