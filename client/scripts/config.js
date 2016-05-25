dcb4548a3210bba2f2caf59c3a16eada04f20035
uinika <uinika@163.com>
2016-05-25 18:43:05 +0800

    Clean build

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -3,10 +3,10 @@
 var Config = angular.module('Config', []);
 
 Config.constant('API', {
-  path: 'http://localhost:8080/drrp/api' //发布
+  // path: 'http://localhost:8080/drrp/api' //发布
   // path: 'http://localhost:5001/api' //本机测试
   // path: 'http://172.16.1.81:8080/api' //周爽
   // path: 'http://172.16.1.77:8080/api' //吴陶冶
-  // path: 'http://172.16.1.78:8080/api' //米兵
+  path: 'http://172.16.1.78:8080/api' //米兵
   // path: 'http://localhost:8080/api' //VPN测试服务器
 });
