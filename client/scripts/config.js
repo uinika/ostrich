2b279920fb4108db58ef007317c10b88f1d071bd
uinika <uinika@163.com>
2016-06-07 18:44:15 +0800

    Update config.js

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -3,5 +3,7 @@
 var Config = angular.module('Config', []);
 
 Config.constant('API', {
-  path: 'http://localhost:8080/drrp/api'
+  // path: 'http://localhost:8080/drrp/api' //发布
+   path: 'http://172.16.1.78:8080/api' //测试1
+  // path: 'http://192.168.9.43:8080/api' //测试2
 });
