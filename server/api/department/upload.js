c221c29e71042a8121e895915122a9b2383eb207
xulan <xulan@chinawiserv.com>
2016-06-13 14:00:14 +0800

    Remove unused mock interface  upload excel

--- server/api/department/upload.js
+++ server/api/department/upload.js
@@ -3,18 +3,18 @@ const Router = require('express').Router(),
   JsonLoader = require('load-json-file'),
   Config = require('../../config');
 
-Router.route('/upload')
-  .get(function(request, response) {
-    let head = {}, body = {};
-
-  });
-
-  Router.route('/upload/excel')
-    .post(function(request, response) {
-      let head = {}, body = {};
-      let datas = JsonLoader.sync(Config.path + 'department/uploadExamples.json');
-      response.json(datas);
-
-    });
+// Router.route('/upload')
+//   .get(function(request, response) {
+//     let head = {}, body = {};
+//
+//   });
+//
+//   Router.route('/upload/excel')
+//     .post(function(request, response) {
+//       let head = {}, body = {};
+//       let datas = JsonLoader.sync(Config.path + 'department/uploadExamples.json');
+//       response.json(datas);
+//
+//     });
 
 module.exports = Router;
