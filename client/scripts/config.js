46984acb5f8b9d6beff10e85946867df7fd0537d
dengtl <dengtl@chinawiserv.com>
2016-06-29 14:51:38 +0800

    modify depart

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -5,6 +5,11 @@ var Config = angular.module('Config', []);
 Config.constant('API', {
   // path: 'http://localhost:8080/drrp/api' //发布
    path: 'http://172.16.1.78:8080/api' //测试1
+  //  path: 'http://172.16.1.63:8080/api' //测试1
   // path: 'http://192.168.9.43:8080/api' //测试2
+<<<<<<< Updated upstream
   // path:'http://localhost:5001/api' //本机测试
+=======
+
+>>>>>>> Stashed changes
 });
